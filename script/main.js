04ecc713ade4f9b1d6e330aae30e8cbdbe5f254a
Victor <vic.inlight@gmail.com>
2023-03-21 14:01:28 +0200

    first class commit

---
+++ script/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// let number = 4.6;
+// const person = "Alexandra";
+// const bool = true;
+// const object = {
+//     name: "John",
+//     age: 25,
+//     famStatus: false
+// };
+//
+// let arr = ["plumb.png", "orange.jpg", 6, "apple.bmp"]
+//
+// const storeName = "Bonus";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ["John", "Alex", "Stephen"],
+//     products: {
+//         apples: 10,
+//         oranges: 15
+//     },
+//     open: true
+// };
+// const result = confirm("Are you here?");
+// const answer = prompt("Are you sure", "yes");
+// const answer = [];
+//
+// answer[0] = prompt("What is your name?", "");
+// answer[1] = prompt("What is your surname?", "");
+// answer[2] = prompt("What is your age?", "");
+
+const isChecked = true,
+    isClose = true;
+
+
+console.log(8 + 8 || 20);
+
+
+
+
